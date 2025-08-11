a897f307270a913892b769bd1562095ca2a28439
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-08-11 19:01:06 +0400

    migrate to localize value

--- src/main/java/com/intellij/velocity/VelocityColorSettingsPage.java
+++ src/main/java/com/intellij/velocity/VelocityColorSettingsPage.java
@@ -18,10 +18,11 @@ package com.intellij.velocity;
 
 import com.intellij.velocity.psi.files.VtlSyntaxHighlighter;
 import consulo.annotation.component.ExtensionImpl;
-import consulo.colorScheme.TextAttributesKey;
+import consulo.apache.velocity.localize.VelocityLocalize;
 import consulo.colorScheme.setting.AttributesDescriptor;
 import consulo.language.editor.colorScheme.setting.ColorSettingsPage;
 import consulo.language.editor.highlight.SyntaxHighlighter;
+import consulo.localize.LocalizeValue;
 import jakarta.annotation.Nonnull;
 
 import static com.intellij.velocity.psi.files.VtlSyntaxHighlighter.*;
@@ -30,76 +31,65 @@ import static com.intellij.velocity.psi.files.VtlSyntaxHighlighter.*;
  * @author Alexey Chmutov
  */
 @ExtensionImpl
-public class VelocityColorSettingsPage implements ColorSettingsPage
-{
-	private static final AttributesDescriptor[] ATTRS;
+public class VelocityColorSettingsPage implements ColorSettingsPage {
+    private static final AttributesDescriptor[] ATTRS;
 
-	static
-	{
-		ATTRS = new AttributesDescriptor[]{
-				createAttributesDescriptor("velocity.color.dot", VELOCITY_DOT),
-				createAttributesDescriptor("velocity.color.parenths", VELOCITY_PARENTHS),
-				createAttributesDescriptor("velocity.color.brackets", VELOCITY_BRACKETS),
-				createAttributesDescriptor("velocity.color.braces", VELOCITY_BRACES),
-				createAttributesDescriptor("velocity.color.operation.sign", VELOCITY_OPERATION_SIGN),
-				createAttributesDescriptor("velocity.color.string", VELOCITY_STRING),
-				createAttributesDescriptor("velocity.color.escape", VELOCITY_ESCAPE),
-				createAttributesDescriptor("velocity.color.number", VELOCITY_NUMBER),
-				createAttributesDescriptor("velocity.color.keyword", VELOCITY_KEYWORD),
-				createAttributesDescriptor("velocity.color.comma", VELOCITY_COMMA),
-				createAttributesDescriptor("velocity.color.semicolon", VELOCITY_SEMICOLON),
-				createAttributesDescriptor("velocity.color.directive", VELOCITY_DIRECTIVE),
-				createAttributesDescriptor("velocity.color.reference", VELOCITY_REFERENCE),
-				createAttributesDescriptor("velocity.color.comment", VELOCITY_COMMENT),
-				createAttributesDescriptor("velocity.color.bad.character", VELOCITY_BAD_CHARACTER),
-				createAttributesDescriptor("velocity.color.scripting.background", VELOCITY_SCRIPTING_BACKGROUND),
-		};
-	}
+    static {
+        ATTRS = new AttributesDescriptor[]{
+            new AttributesDescriptor(VelocityLocalize.velocityColorDot(), VELOCITY_DOT),
+            new AttributesDescriptor(VelocityLocalize.velocityColorParenths(), VELOCITY_PARENTHS),
+            new AttributesDescriptor(VelocityLocalize.velocityColorBrackets(), VELOCITY_BRACKETS),
+            new AttributesDescriptor(VelocityLocalize.velocityColorBraces(), VELOCITY_BRACES),
+            new AttributesDescriptor(VelocityLocalize.velocityColorOperationSign(), VELOCITY_OPERATION_SIGN),
+            new AttributesDescriptor(VelocityLocalize.velocityColorString(), VELOCITY_STRING),
+            new AttributesDescriptor(VelocityLocalize.velocityColorEscape(), VELOCITY_ESCAPE),
+            new AttributesDescriptor(VelocityLocalize.velocityColorNumber(), VELOCITY_NUMBER),
+            new AttributesDescriptor(VelocityLocalize.velocityColorKeyword(), VELOCITY_KEYWORD),
+            new AttributesDescriptor(VelocityLocalize.velocityColorComma(), VELOCITY_COMMA),
+            new AttributesDescriptor(VelocityLocalize.velocityColorSemicolon(), VELOCITY_SEMICOLON),
+            new AttributesDescriptor(VelocityLocalize.velocityColorDirective(), VELOCITY_DIRECTIVE),
+            new AttributesDescriptor(VelocityLocalize.velocityColorReference(), VELOCITY_REFERENCE),
+            new AttributesDescriptor(VelocityLocalize.velocityColorComment(), VELOCITY_COMMENT),
+            new AttributesDescriptor(VelocityLocalize.velocityColorBadCharacter(), VELOCITY_BAD_CHARACTER),
+            new AttributesDescriptor(VelocityLocalize.velocityColorScriptingBackground(), VELOCITY_SCRIPTING_BACKGROUND),
+        };
+    }
 
-	private static AttributesDescriptor createAttributesDescriptor(String displayNameKey, TextAttributesKey textAttributesKey)
-	{
-		return new AttributesDescriptor(VelocityBundle.message(displayNameKey), textAttributesKey);
-	}
-
-	@Override
+    @Override
     @Nonnull
-	public String getDisplayName()
-	{
-		return "Velocity";
-	}
+    public LocalizeValue getDisplayName() {
+        return VelocityLocalize.vtlLanguageDisplayName();
+    }
 
-	@Override
+    @Override
     @Nonnull
-	public AttributesDescriptor[] getAttributeDescriptors()
-	{
-		return ATTRS;
-	}
+    public AttributesDescriptor[] getAttributeDescriptors() {
+        return ATTRS;
+    }
 
-	@Override
+    @Override
     @Nonnull
-	public SyntaxHighlighter getHighlighter()
-	{
-		return new VtlSyntaxHighlighter();
-	}
+    public SyntaxHighlighter getHighlighter() {
+        return new VtlSyntaxHighlighter();
+    }
 
-	@Override
+    @Override
     @Nonnull
-	public String getDemoText()
-	{
-		return "#* comment  \n" +
-				"  comment *#\n" +
-				"$reference.method('string').property\n" +
-				"## another comment\n" +
-				"#foreach($loopvar in [-4..77])\n" +
-				"  #set($var = {1:\"double-quoted ${loopvar.toString()}\", 2:false #fff})\n" +
-				"#{end}\n\n" +
-				"#{macro}(m1 $p1, $p2 $p3 $)\n" +
-				"  #if($p1 * ${p2} eq $p3)\n" +
-				"#templatetext\n" +
-				"  #{else}template text\n" +
-				"  #end\n" +
-				"#end\n" +
-				"#m1(4 5 7)\n" +
-				"#{m1}(8, 7, 6)\n";
-	}
+    public String getDemoText() {
+        return "#* comment  \n" +
+            "  comment *#\n" +
+            "$reference.method('string').property\n" +
+            "## another comment\n" +
+            "#foreach($loopvar in [-4..77])\n" +
+            "  #set($var = {1:\"double-quoted ${loopvar.toString()}\", 2:false #fff})\n" +
+            "#{end}\n\n" +
+            "#{macro}(m1 $p1, $p2 $p3 $)\n" +
+            "  #if($p1 * ${p2} eq $p3)\n" +
+            "#templatetext\n" +
+            "  #{else}template text\n" +
+            "  #end\n" +
+            "#end\n" +
+            "#m1(4 5 7)\n" +
+            "#{m1}(8, 7, 6)\n";
+    }
 }
